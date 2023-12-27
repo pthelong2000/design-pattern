804c9fa5d31200397765cd007add2f137d9af9a5
LongPT <p.thelong2000@gmail.com>
2023-12-27 21:38:01 +0700

    create memento

---
+++ src/behavioral/memento/Main.java
@@ -0,0 +1,63 @@
+package behavioral.memento;
+
+public class Main {
+    public static void main(String[] args) {
+        Originator originator = new Originator();
+        Caretaker caretaker = new Caretaker();
+
+        originator.setState("State 1");
+        caretaker.setMemento(originator.saveStateToMemento());
+
+        originator.setState("State 2");
+        originator.restoreStateFromMemento(caretaker.getMemento());
+        System.out.println("Current State: " + originator.getState());
+
+    }
+}
+
+// Memento
+class Memento {
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
+
+// Caretaker
+class Caretaker {
+    private Memento memento;
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+
+    public Memento getMemento() {
+        return memento;
+    }
+}
+
+// Originator
+class Originator {
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Memento saveStateToMemento() {
+        return new Memento(state);
+    }
+
+    public void restoreStateFromMemento(Memento memento) {
+        state = memento.getState();
+    }
+}
